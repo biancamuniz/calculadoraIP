c9713f757b74c594c092a7f32b38abbdeaada609
biancamuniz <biancarolmuniz@gmail.com>
2018-06-29 16:59:11 -0300

    Add files via upload

---
+++ trabs/index.php
@@ -0,0 +1,40 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" type="text/css" href="semantic/semantic.css">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body>
+
+    <h1 align="center" class="h1"> CALCULADORA IP </h1>
+
+
+    <div class="ui form">
+        <div class="fields">
+            <div class="one wide field">
+                <input type="number" id="ip1">
+            </div>
+            <div class="one wide field">
+                <input type="number" id="ip2">
+            </div>
+            <div class="one wide field">
+                <input type="number" id="ip3">
+            </div>
+            <div class="one wide field">
+                <input type="number" id="ip4">
+            </div>
+            <div class="one wide field">
+                <input type="number" id="mascara" min="24" max="32">
+            </div>
+
+        </div>
+        <button class="ui olive button" type="submit">Submit</button>
+    </div>
+
+</body>
+</html>
